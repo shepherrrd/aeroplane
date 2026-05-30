a39709f6810bf250960798f8ccb93ef8dded5d03
Akinkunmi <akinkunmioye42@gmail.com>
2026-05-30 07:36:36 +0100

    Remove manual root route

--- src/client/routes/root.tsx
+++
@@ -1,6 +0,0 @@
-import { createRootRoute } from "@tanstack/react-router";
-import { RootShell } from "../components/layout/root-shell";
-
-export const rootRoute = createRootRoute({
-  component: RootShell
-});
